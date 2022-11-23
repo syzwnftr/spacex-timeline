0424e9bed13037c8950812952ac10c4144070182
Syazwan Fitri <syazfitry69@gmail.com>
2022-11-24 04:16:23 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="blue-grey" dark>
+      <div class="d-flex align-center">
+        <h2>SpaceX Timeline</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-timeline v-if="launches.length">
+          <LaunchTimelineItem
+            v-for="launch in launches"
+            :key="launch.flight_number"
+            :launch="launch"
+          />
+        </v-timeline>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LaunchTimelineItem from "./components/LaunchTimelineItem.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: { LaunchTimelineItem },
+  data() {
+    return {
+      launches: [],
+    };
+  },
+  async created() {
+    const res = await fetch("https://api.spacexdata.com/v3/launches");
+    const data = await res.json();
+    data.forEach((launch) => {
+      this.launches.push(launch);
+    });
+    console.log(this.launches);
+  },
+};
+</script>
